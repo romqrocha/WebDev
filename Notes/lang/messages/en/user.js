fc1a920a033d2c41fc9049124fa722647e8e319b
Rodrigo <rodrigo@vancouver4us.com>
2025-09-08 11:56:37 -0700

    Notes webapp file structure

---
+++ Notes/lang/messages/en/user.js
@@ -0,0 +1,26 @@
+class MessagesEn {
+  static msgs = {
+    home: {
+      title: "Local Note Taker",
+      subtitle: "Everything is stored locally!",
+      writeButton: "Start Writing",
+      readButton: "Read Your Notes",
+    },
+    redirect: {
+      redirectMsg: "Redirecting..."
+    },
+    writer: {
+      title: "Write Notes",
+      storedMsg: "Last stored at: ",
+      addButton: "Add",
+      removeButton: "Remove",
+      backButton: "Back",
+    },
+    reader: {
+      title: "Read Notes",
+      updatedMsg: "Last updated at: ",
+      emptyStorage: "You haven't taken any notes yet...",
+      backButton: "Back",
+    },
+  };
+}
